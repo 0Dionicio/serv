0eb06420f07775099206c73a984c0d44e4f7a6a8
0Dionicio <90216484+0Dionicio@users.noreply.github.com>
2025-07-29 09:24:28 -0600

    Update api.js

--- api.js
+++ api.js
@@ -41,6 +41,31 @@ app.get('/orden/:id',async (req, res) => {
 
 });
 
+app.get('/orders/:id',async (req, res) => {
+    const userId = req.params.id;
+
+    let connection;
+    try {
+        connection = await mysql.createConnection(dbConfig);
+        const [rows] = await connection.execute("SELECT `orden`, `deporte`, `modelo`, `posicion`, `medida`, `lanzador`, `modelo-canasta`, `modelo-palma`, `modelo-logo`, `latigo-dividido`,`latigo`, `pieza-2-dividida`, `pieza-1`, `textura-pieza-1`, `pieza-2`, `textura-pieza-2`, `pieza-2-2`, `textura-pieza-2-2`, `pieza-3`, `textura-pieza-3`, `pieza-4`, `textura-pieza-4`, `pieza-5`, `textura-pieza-5`, `pieza-6`, `textura-pieza-6`, `pieza-7`, `textura-pieza-7`, `pieza-8`, `textura-pieza-8`, `pieza-9`, `textura-pieza-9`, `pieza-10`, `textura-pieza-10`, `pieza-11`, `textura-pieza-11`, `color-canasta`, `vivos`, `vivos-palma`, `ribete`, `latiguillos`, `hilos`, `correa-lateral`, `correa-interna`, `correa-latigo`, `correa-dedos`, `correa-canasta`, `palma`, `palma-interior`, `contorno-logo`, `logo`, `logo-detalle`, `codigo` FROM gloves WHERE client_id = ?", [userId]);
+        
+        if (rows.length === 0){
+            return res.status(404).json({error: 'usuario no encontrado'});
+        }
+        res.set('Cache-Control', 'no-store');
+        res.json(rows[0]);
+    } catch (error){
+        console.error('Error BD:',error);
+        res.status(500).json({error: 'Error interno del servidor' });
+    }finally{
+        if (connection){
+            await connection.end();
+        }
+    }
+
+});
+
+
 app.get('/', (req, res)=>{
     res.send('API RES Node.js con MySQL y encabezados HTTP personalizados');
 });

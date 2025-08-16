896bc49cd66e04a60972d92114442585db8eabdd
0Dionicio <90216484+0Dionicio@users.noreply.github.com>
2025-08-16 08:23:03 -0600

    Update api.js

--- api.js
+++ api.js
@@ -65,6 +65,30 @@ app.get('/code-all/:id',async (req, res) => {
 
 });
 
+app.get('/filter/:id',async (req, res) => {
+    const userId = req.params.id;
+
+    let connection;
+    try {
+        connection = await mysql.createConnection(dbConfig);
+        const [rows] = await connection.execute("SELECT * FROM gloves WHERE tag = ?", [userId]);
+        
+        if (rows.length === 0){
+            return res.status(404).json({error: 'usuario no encontrado'});
+        }
+        res.set('Cache-Control', 'no-store');
+        res.json(rows[0]);
+    } catch (error){
+        console.error('Error BD:',error);
+        res.status(500).json({error: 'Error interno del servidor' });
+    }finally{
+        if (connection){
+            await connection.end();
+        }
+    }
+
+});
+
 app.get('/id/:id',async (req, res) => {
     const userId = req.params.id;
 
